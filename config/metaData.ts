e3f19ed41a63f84728db94ca41b54b2dabe7c733
howdy-mj <hi.minjungkim@gmail.com>
2022-09-12 19:55:20 +0900

    metaData.ts > siteUrl 수정2

--- config/metaData.ts
+++ config/metaData.ts
@@ -3,7 +3,7 @@ const metaData = {
   name: 'MJ Kim',
   description: '안녕하세요, 기록하고 공유하기를 좋아하는 프론트엔드 개발자 김민정입니다.',
   language: 'ko',
-  siteUrl: 'https://howdy-blog-v2.vercel.app/',
+  siteUrl: 'https://howdy-blog-v2.vercel.app',
   socialBanner: '/images/socialBanner.jpg',
   logoImage: '/images/logo.png',
   email: 'hi.minjungkim@gmail.com',
