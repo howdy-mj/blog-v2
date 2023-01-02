cbcaeb1124094b5f84555cb081fc8b84b4bc720f
MJ <hi.minjungkim@gmail.com>
2023-01-02 21:43:17 +0900

    feat: @next/codemod 설치

--- src/components/PersonalInformation.tsx
+++ src/components/PersonalInformation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import metaData from '@config/metaData';
 import styled from 'styled-components';
 import { Text } from '@components/Typography.style';
