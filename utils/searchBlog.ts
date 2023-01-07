2271952c9c241299570604e81ed4b3d659293f3b
MJ <hi.minjungkim@gmail.com>
2023-01-07 17:27:42 +0900

    searchBlog.ts > tag 수정

--- utils/searchBlog.ts
+++ utils/searchBlog.ts
@@ -13,11 +13,10 @@ export const searchBlogPosts = (searchValue: string, originPosts: Content[]) => 
     return originPosts;
   }
 
-  const tag = searchValue.slice(1);
   return getSearchedResult(
     searchValue,
     posts.filter((post) => {
-      const matchedTag = post.frontMatter.tags?.includes(tag);
+      const matchedTag = post.frontMatter.tags?.includes(searchValue);
       const matchedTitle = post.frontMatter.title.toLowerCase().includes(searchValue);
       const matchedSummary = post.frontMatter.summary?.includes(searchValue);
       const nonDraft = post.frontMatter.draft === undefined || !post.frontMatter.draft;
