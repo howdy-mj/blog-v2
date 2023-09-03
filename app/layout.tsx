74e6e6bb75b486b0157880c35143e426350a1d5f
MJ <hi.minjungkim@gmail.com>
2023-09-03 17:38:15 +0900

    styled-components 업데이트 및 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from 'react';
 import metaData from '@config/metaData';
+import StyledComponentsRegistry from '@lib/registry';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang={metaData.language}>
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
