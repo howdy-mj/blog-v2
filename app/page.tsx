11c01a77db2bdffe13cd49e9dd7a465a9fff7ffe
MJ <hi.minjungkim@gmail.com>
2023-09-03 17:59:32 +0900

    데이터 불러오기 및 UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { Metadata } from 'next';
 import metaData from '@config/metaData';
+import { filterDraftInProduction, getAllFiles } from '@utils/getFiles';
+import { getCurrentPagePosts } from '@utils/makeBlogPage';
+import { getAllTags } from '@utils/getAllTags';
+import { makePostsRss } from '@utils/makePostsRss';
+import BlogPostsContainer from 'components/BlogPostsContainer';
 
 // TODO: SEO
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
@@ -8,5 +13,23 @@ export const metadata: Metadata = {
 };
 
 export default async function MainPage() {
-  return <>Main</>;
+  const { originPosts, totalPosts, tags } = await getAllPostsAndTagsData();
+  return <BlogPostsContainer originPosts={originPosts} tags={tags} totalPosts={totalPosts} />;
 }
+
+const getAllPostsAndTagsData = async () => {
+  const allPosts = filterDraftInProduction(getAllFiles('posts'));
+  const totalPosts = allPosts.length;
+  const posts = getCurrentPagePosts(allPosts, 1);
+  const tags = getAllTags();
+
+  if (totalPosts > 0) {
+    await makePostsRss(allPosts);
+  }
+
+  return {
+    originPosts: posts,
+    tags,
+    totalPosts,
+  };
+};
