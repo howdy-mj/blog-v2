f3bb3c3599863eebfc8ce5c717525a02e8aea721
MJ <hi.minjungkim@gmail.com>
2023-09-03 17:52:13 +0900

    MainLayout.tsx 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react';
 import metaData from '@config/metaData';
 import StyledComponentsRegistry from '@lib/registry';
+import MainLayout from 'components/MainLayout';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang={metaData.language}>
       <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <MainLayout>{children}</MainLayout>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
