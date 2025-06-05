eab3c2489a948d3257ead335a943caf7830b4fc6
MJ <hi.minjungkim@gmail.com>
2025-06-05 23:19:09 +0900

    fix: robots 수정

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -3,12 +3,14 @@ import metaData from '@config/metaData';
 
 const siteUrl = metaData.siteUrl;
 
+const robotsTxt = `
+User-agent: *
+Allow: /
+Disallow: /api/
+Sitemap: ${siteUrl}/sitemap.xml
+`;
+
 export default function robotsHandler(req: NextApiRequest, res: NextApiResponse) {
-  res.send(`
-    User-agent: *
-    Allow: /
-    Disallow: /api
-    Sitemap: ${siteUrl}/sitemap.xml
-    Host: ${siteUrl}
-  `);
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send(robotsTxt.trim());
 }
